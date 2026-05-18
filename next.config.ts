51cca22129760e10f1de9bca9dca3cf89b1e6ba6
ibrahimoll <ibrahimolleik3@gmail.com>
2026-05-18 13:38:13 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.0.110'],
+  allowedDevOrigins: ["192.168.0.110"],
 };
 
 export default nextConfig;
